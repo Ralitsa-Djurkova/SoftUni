c317607a3d3339e8bf28706d8ab0fd27a3f6e2fa
Ralitsa Djurkova <ralitsadj@gmail.com>
2021-01-08 10:30:10 +0200

    Added Task 4

---
+++ Programing-Basics/charityCampaign/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace charityCampaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int bakers = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            double cakesProfit = cakes * 45 ;
+            double wafflesProfit = waffles * 5.80 ;
+            double pancakesProfit = pancakes * 3.20;
+           
+
+            double profitPerDayByOne = cakesProfit + wafflesProfit + pancakesProfit;
+            double profitPerDayByAll = profitPerDayByOne * bakers;
+            
+            double totalProfit = profitPerDayByAll * days;
+            
+            double expenses = totalProfit / 8;
+
+            double finalProfit = totalProfit - expenses;
+
+            Console.WriteLine($"{finalProfit:F2}");
+        }
+    }
+}
