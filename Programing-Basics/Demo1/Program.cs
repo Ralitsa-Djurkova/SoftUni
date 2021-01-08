4e0c3a44c196130a74e6a40537bc643af1ab50bf
Ralitsa Djurkova <ralitsadj@gmail.com>
2021-01-08 10:31:28 +0200

    Added 05.task

---
+++ Programing-Basics/Demo1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+              
+            //3. print format bgn -> 2 
+
+            double usd = double.Parse(Console.ReadLine());
+            double bgn  = usd * 1.79549;
+            Console.WriteLine($"{bgn:F2}");
+
+        }
+    }
+}
