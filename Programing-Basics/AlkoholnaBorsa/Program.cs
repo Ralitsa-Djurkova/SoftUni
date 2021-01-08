a19718e58a4319834fcf22575666e51d8f531cb8
Ralitsa Djurkova <ralitsadj@gmail.com>
2021-01-08 10:26:41 +0200

    Added Programming Basics

---
+++ Programing-Basics/AlkoholnaBorsa/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AlkoholnaBorsa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+           
+
+            double whiskeyPrice = double.Parse(Console.ReadLine());
+            double beerLiters = double.Parse(Console.ReadLine());
+            double wineLiters = double.Parse(Console.ReadLine());
+            double rakiaLiters = double.Parse(Console.ReadLine());
+            double whiskeyLiters = double.Parse(Console.ReadLine());
+
+            double rakiaPrice = whiskeyPrice / 2;
+            double winePrice = rakiaPrice - 0.4 * rakiaPrice;
+            double beerPrice = rakiaPrice - 0.8 * rakiaPrice;
+
+            double totalPrice = (rakiaPrice * rakiaLiters) + (winePrice * wineLiters) + (whiskeyPrice * whiskeyLiters) + (beerLiters * beerPrice);
+
+            Console.WriteLine($"{totalPrice:F2}");
+
+
+
+            
+            
+        }
+    }
+}
